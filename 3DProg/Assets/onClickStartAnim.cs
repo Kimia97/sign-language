2403e0bd054ff8e965b031d4a44a192100bb4610
Brogzybro <bragehs@hotmail.com>
2018-11-06 14:27:42 +0100

    Adding project
    adding project to git

---
+++ 3DProg/Assets/onClickStartAnim.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onClickStartAnim : MonoBehaviour {
+
+    public AnimationClip animationClip;
+    public Animator animator;
+
+
+
+	// Use this for initialization
+	void Start () {
+        animator.SetBool("playH", true);
+        Debug.Log("button pressed");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+    }
+}
